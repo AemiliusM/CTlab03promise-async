34e79ba34e6b9708e82bc593e99d68da36451c22
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-09-15 18:16:38 -0700

    got first test not passing still

--- __test__/simpleDb.test.js
+++ __test__/simpleDb.test.js
@@ -4,17 +4,17 @@ import SimpleDb from '../lib/simpleDb.js';
 describe('simpleDb creates files, as well as reads and copies them', () => {
   const destination = '../store';
 
-//   beforeEach(() => {
-//     return rm(destination, { recursive: true, force: true }).then(() => {
-//       return mkdir(destination, { recursive: true });
-//     });
-//   });
+  beforeEach(() => {
+    return rm(destination, { recursive: true, force: true }).then(() => {
+      return mkdir(destination, { recursive: true });
+    });
+  });
 
   it('it tests if a file is made', () => {
     const dir = new SimpleDb(destination);
     const file = { stuff: 'file1.txt' };
-    return dir.save(file).then((saved) => {
-      expect(saved).toEqual(file);
+    return dir.save(file).then((file) => {
+      expect(file.id).toEqual(expect.any(String));
     });
   });
 });
